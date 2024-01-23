4441cbd397cdc5202cb6bf094afe811539c5d936
rojenzaman <rojen@riseup.net>
2024-01-23 09:07:05 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 cd "$(dirname "$0")"
 TAG="$(date +%Y-%m-%d)"
-docker build . --tag rojen/php8.3-apache:latest --tag rojen/php8.3-apache:${TAG}
-docker push rojen/php8.3-apache:latest
-docker push rojen/php8.3-apache:${TAG}
+VERSION="8.3"
+
+docker build . --tag rojen/php${VERSION}-apache:latest --tag rojen/php${VERSION}-apache:${TAG}
+docker push rojen/php${VERSION}-apache:latest
+docker push rojen/php${VERSION}-apache:${TAG}
